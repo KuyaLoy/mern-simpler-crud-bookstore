ae6b571113cd7d60da6ea6a9cbb76f2d9acce7cd
Robin Tapiru <robintapiru0894@gmail.com>
2024-03-21 17:50:40 +0400

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 6969;
+
+export const mongoDBURL =
+  "mongodb+srv://blogAdmin:blogAdmin12345@mern-blog.4abygtj.mongodb.net/mern-bookstore?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import { mongoose } from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoutes from "./routes/bookRoute.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "POST", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
+});
+
+app.use("/books", booksRoutes);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
